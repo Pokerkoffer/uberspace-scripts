047b588f780ce3202f40ad7665b86a7b789ca117
Pokerkoffer <Pokerkoffer@users.noreply.github.com>
2017-02-07 12:27:37 +0100

    debug removed

--- roundcube/plugins/quota_notify/quota_notify.php
+++ roundcube/plugins/quota_notify/quota_notify.php
@@ -7,8 +7,6 @@ class quota_notify extends rcube_plugin {
   }
 
   function quota_warning_message($args) {
-    file_put_contents('./log_'.date("j.n.Y").'.txt', print_r($args,true), FILE_APPEND);
-
     $rcmail = rcmail::get_instance();
     if($args['percent'] > 99) {
       $rcmail->output->show_message('Deine Mailbox ist voll! Du kannst keine weiteren E-Mails mehr empfangen. Lösche Mails mit großen Anhängen und leere deinen Papierkorb!', 'error');
