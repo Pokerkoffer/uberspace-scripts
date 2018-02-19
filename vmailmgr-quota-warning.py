9caedf4ee0dd834eb426948c62edce434d9c5c99
Pokerkoffer <Pokerkoffer@users.noreply.github.com>
2018-02-19 19:24:33 +0100

    update

---
+++ vmailmgr-quota-warning.py
@@ -0,0 +1,71 @@
+import argparse
+import datetime
+import ntpath
+import os
+import re
+# TODO
+# get du of all dirs in mailDir
+# get dumpvuser atts
+# compare realSize with quota
+# if realsize > quota:
+# symlink mailbox_nearly_full message to users /new folder
+import subprocess
+import sys
+
+import PathType
+
+
+def create_symlink(dest, message_file):
+    file_name = ntpath.basename(message_file.name)
+    os.symlink(message_file.name, os.path.join(dest, file_name))
+
+
+def get_directory_sizes(dir):
+    result = subprocess.run(['du -s ' + dir], shell=True, stdout=subprocess.PIPE)
+    p = re.compile(r'^[\s]+|([\w,\.]+)[\s]+(.+)$', re.MULTILINE)
+    output = result.stdout.decode('utf8')
+    matches = p.findall(output)
+    for index, (first, last) in enumerate(matches):
+        if first == '' or last == '' or not os.path.isdir(last):
+            matches.pop(index)
+    return matches
+
+
+def get_quota_sizes(dir):
+    dirs = os.listdir(dir)
+    return [entry for entry in dirs if os.path.isdir(os.path.join(dir, entry))]
+
+
+def main(args):
+    print(args.file)
+    print(args.dir)
+    file = args.file[0]
+    dir_sizes = get_directory_sizes(args.dir[0])
+    user_quota_sizes = get_quota_sizes(args.dir[0])
+    
+    user_dir_sizes_and_quota = [('','','')]
+    for (size, path, quota) in user_dir_sizes_and_quota:
+        if int(size) >= quota:
+            create_symlink(path, file)
+
+    write_logfile()
+
+
+def write_logfile():
+    now = datetime.datetime.now()
+    time = now.strftime("%d.%m.%Y %H:%M:%S")
+    logfile = open('./lastrun', 'w')
+    logfile.write(time)
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    parser = argparse.ArgumentParser(description='Help')
+    parser.add_argument('-f', '--file', metavar='path', nargs=1, required=True, type=argparse.FileType('r'),
+                        help='The file with the warning message')
+    parser.add_argument('-d', '--dir', metavar='dir', nargs=1, required=True,
+                        type=PathType.PathType(exists=True, type='dir'),
+                        help='The directory to count folder sizes from')
+    args = parser.parse_args(args)
+    print(args)
+    main(args)
